66f80ca21fc2175c87e0b25697ad24e41d99b4ee
Ryan Campbell <ryancampbell@ryan-campbells-macbook-pro.local>
2009-03-02 16:24:36 -0500

    Adding new php tutorials to github

---
+++ php/tutorial/16-collectdigits.php
@@ -0,0 +1,12 @@
+<?php
+
+answer();
+
+$result=prompt( "Hello.  Please enter any number", array( "choices" => "[DIGITS]" ) );
+
+if ($result->name=='choice') say( "Great, you said " + $result->value );
+	
+hangup();
+
+?>
+
