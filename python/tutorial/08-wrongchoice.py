c83edaabb196673a1c7826a1fad0602aac6b5ecf
Dan Evers <dlevers@dan-everss-macbook-pro.local>
2009-03-03 13:09:30 -0500

    Removed repeat option

--- python/tutorial/08-wrongchoice.py
+++ python/tutorial/08-wrongchoice.py
@@ -5,15 +5,15 @@
 
 answer()
 
-result = ask("Hi. For sales, say sales or press 1. For support, say support or press 2", {'choices':"sales(1,sales), support(2,support)", 'repeat':3})
+result = ask("Hi. For sales, say sales or press 1. For support, say support or press 2", {'choices':"sales(1,sales), support(2,support)"})
 
 if (result.name == 'choice'):
-    if (result.value == "sales"): 
+    if (result.value == "sales"):
         say("Ok, let me transfer you to sales")
-	transfer("14129272358")
-    if (result.value == "support"): 
+        transfer("14129272358")
+    if (result.value == "support"):
         say("Ok, let me transfer you to support")
-	transfer("14129272341")
+        transfer("14129272341")
 
 if (result.name == 'badChoice'):
     say("I'm not sure what you wanted. Goodbye.")
