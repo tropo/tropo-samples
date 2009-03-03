98509ec33b8490cf68f2fe484146417a6b153ca8
Dan York <dyork@Lodestar2.com>
2009-03-03 00:40:31 -0800

    Adding tutorial 18 and 19 for groovy, javascript, php and ruby

---
+++ groovy/tutorial/18-playaudio.groovy
@@ -0,0 +1,15 @@
+// -----------------
+// Tutorial 18 - playing audio files
+// -----------------
+
+answer();
+
+say( "This is read using a text to speech engine.");
+
+say( "But this is recorded audio from a URL... http://evolution.voxeo.com/library/audio/prompts/common/Hello.wav");
+
+say( "Or you can just play the recorded audio URL...");
+
+say( "http://evolution.voxeo.com/library/audio/prompts/common/Hello.wav" );
+
+hangup();
---
+++ javascript/tutorial/19-recordaudio.js
@@ -0,0 +1,25 @@
+// -----------------
+// Tutorial 18 - recording audio input
+// -----------------
+
+answer();
+
+// record a message.  Play the beep, caller must speak within 10 seconds.  
+// Allow 7 seconds of silence and up to 60 seconds of recording.
+
+result=record( "Hello.  Thanks for calling.  Leave your message at the beep.",
+  				{ beep:true, timeout:10, silenceTimeout:7, maxTime:60 } 		);
+
+
+if ( result.name=='record') 
+{ 
+	// record returns a URI pointing to the recorded audio...
+	
+	log( "result.recordURI = " + result.recordURI );
+	
+	// we can then use that URI in a say command to play the recorded audio back...
+	
+	say( "you said " + result.recordURI );
+}
+
+hangup();
