041d04b6a323d6f33f694f34c14a2fc45b656df6
Alex Agranovsky <alex@SolitaryShell-2.local>
2009-03-03 16:03:39 -0500

    Fixed callerID/calledID format on outgoing call sample

--- ruby/outgoingcall.rb
+++ ruby/outgoingcall.rb
@@ -3,16 +3,16 @@
  
 answer
 options = { :answerOnMedia => false,
-            :callerID      => 'tel:+4157044517',
+            :callerID      => '+14157044517',
             :timeout       => 12.123,
             :onAnswer      => lambda { |event| log '******************** Answered from ' + event.inspect.to_s },
             :onError       => lambda { |event| log '******************** oops , error*********************' + event.inspect.to_s },
             :onTimeout     => lambda { |event| log '******************** timeout *********************' + event.inspect.to_s },
-            :onCallFailure => lamdba { |event| log '******************** call failed *********************' + event.inspect.to_s }
+            :onCallFailure => lambda { |event| log '******************** call failed *********************' + event.inspect.to_s }
           }
 
-event = call 'tel:14155551212', options
+event = call 'tel:+14155551212', options
 
 if event.name == 'answer'
-  log 'Outgoing call gets answered by ' + event.value.calleeID
+  log 'Outgoing call gets answered by ' + event.value.calledID
 end
