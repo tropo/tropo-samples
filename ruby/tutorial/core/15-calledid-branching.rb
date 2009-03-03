62756c163bc245d436237199ae3ed5f1ca08fc84
Jason Goecke <jason@adhearsion.com>
2009-03-03 17:25:06 +0000

    Changed 13-14 of the Ruby examples to reflect the global variable

--- ruby/tutorial/core/15-calledid-branching.rb
+++ ruby/tutorial/core/15-calledid-branching.rb
@@ -4,7 +4,7 @@
 
 answer
 
-if currentCall.calledID == '4075551111' { say 'Hello Andrew.' }
-if currentCall.calledID == '4075552222' { say 'Hello Brian.' }
+if $currentCall.calledID == '4075551111' { say 'Hello Andrew.' }
+if $currentCall.calledID == '4075552222' { say 'Hello Brian.' }
 
 hangup
