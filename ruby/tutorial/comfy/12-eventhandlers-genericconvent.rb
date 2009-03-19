cafeed22063c782586f44bc7487adf4c09cf16ab
Jason Goecke <jason@goecke.net>
2009-03-19 09:35:01 -0700

    Modifications to the Ruby comfy example 12

--- ruby/tutorial/comfy/12-eventhandlers-genericconvent.rb
+++ ruby/tutorial/comfy/12-eventhandlers-genericconvent.rb
@@ -1,35 +1,36 @@
 # -----------
 # using the generic onEvent handler instead
 # -----------
-
-answer
+call = Tropo::TropoCall.new($incomingCall)
+call.answer
 
 prompt  = 'Hi. For sales, just say sales or press 1. For support, say support or press 2.'
 choices = 'sales( 1, sales), support( 2, support)'
 
-ask prompt, :choices => choices, :repeat => 3 do |link|
+call.ask prompt, :choices => choices, :repeat => 3 do |link|
 
   link.on :choice do |event|
     case event.value
     when 'sales'
-      say 'Ok, let me transfer you to sales.'
+      call.say 'Ok, let me transfer you to sales.'
       transfer '14129272358'
     when 'support'
-      say 'Sure, let me get support.  Please hold.'
+      call.say 'Sure, let me get support.  Please hold.'
       transfer '14129272341'
     else
+      call.say 'An error has occurred, one moment please.'
       transfer '14129272358'
     end
   end
   
   link.on :bad_choice do |event|
-    say 'I am sorry, I did not understand what you said.'
+    call.say 'I am sorry, I did not understand what you said.'
   end
   
   link.on :timeout do |event|
-    say 'I am sorry. I did not hear anything.'
+    call.say 'I am sorry. I did not hear anything.'
   end
 
 end
 
-hangup
+call.hangup
