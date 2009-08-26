4f97d2582a2b4fd4a36c37f0005366b8ba1a836f
Jason Goecke <jsgoecke@jsgoecke-tropo.local>
2009-08-26 15:57:44 -0700

    Added an example to cover the startCallRecording and stopCallRecording methods

---
+++ ruby/record-start-stop.rb
@@ -0,0 +1,12 @@
+answer
+ 
+say 'Thank you for calling the recording service.'
+startCallRecording "http://tropo-audiofiles-to-s3.heroku.com/post_audio_to_s3?filename=start-stop-test.wav"
+ 
+prompt 'Do you like chocolate sundaes?', { :choices => 'yes no' }
+ 
+say 'Great!'
+stopCallRecording
+say 'Thank you for using the recording service.'
+ 
+hangup
