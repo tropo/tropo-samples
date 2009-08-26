9954ee6952bb7f8f30d39d2637b4d7eceda14fa8
Jason Goecke <jsgoecke@jsgoecke-tropo.local>
2009-08-26 16:27:10 -0700

    Added Groovy example for start/stop callrecording

---
+++ groovy/record-start-stop.groovy
@@ -0,0 +1,12 @@
+answer()
+
+say('Welcome to the Tropo recording service.')
+startCallRecording("http://tropo.a-domain.com/post_audio?filname=mynewfile.wav")
+
+prompt('Welcome to groovy recording test', [choices: 'yes no'])
+
+say('Great!')
+stopCallRecording
+say('Thank you for using the Tropo recording service. Goodbye.')
+
+hangup()
