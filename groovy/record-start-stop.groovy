9b3fcf1efba2d0e7635d6e7ed8f88fe34b7ece68
Jason Goecke <jsgoecke@jsgoecke-tropo.local>
2009-08-26 17:57:05 -0700

    Fixed the grammars for yes/no in Ruby and Groovy example

--- groovy/record-start-stop.groovy
+++ groovy/record-start-stop.groovy
@@ -3,7 +3,7 @@ answer()
 say('Welcome to the Tropo recording service.')
 startCallRecording("http://tropo.a-domain.com/post_audio?filname=mynewfile.wav")
 
-prompt('Welcome to groovy recording test', [choices: 'yes no'])
+prompt('Welcome to groovy recording test', [choices: 'yes, no'])
 
 say('Great!')
 stopCallRecording
--- ruby/record-start-stop.rb
+++ ruby/record-start-stop.rb
@@ -3,7 +3,7 @@ answer
 say 'Thank you for calling the recording service.'
 startCallRecording "http://tropo-audiofiles-to-s3.heroku.com/post_audio_to_s3?filename=start-stop-test.wav"
  
-prompt 'Do you like chocolate sundaes?', { :choices => 'yes no' }
+prompt 'Do you like chocolate sundaes?', { :choices => 'yes, no' }
  
 say 'Great!'
 stopCallRecording
