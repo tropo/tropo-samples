8fe6884d11cd191250f3e9247f5dab665ceb9ba7
Jason <jason@goecke.net>
2010-05-06 08:06:15 -0700

    Fixed the example to properly resond with the onChoice

--- ruby/favbasketballteam.rb
+++ ruby/favbasketballteam.rb
@@ -7,41 +7,13 @@ require 'rexml/document'
 answer
 
 options = { 
-    'repeat'=>2,
-	'choices'=>"Miami Heat (miami, heat, miami heat, 1),\n Orlando Magic (Orlando, Magic, Orlando Magic),\nBoston Celtics (Boston, Boston Celtics, Celtics),\nPhiladelphia Seventy Sixers (Philadelphia, Seventy Sixers, Philadelphia Seventy Sixers),\nNew Jersey Nets (New Jersey, Nets, New Jersey Nets),\n New York Knicks (New York, New York Knicks, Knicks),\n Toronto Raptors(Toronto, Raptors, Toronto Raptor),\n Cleveland Cavaliers (Cleveland, Cavaliers, Cleveland Cavaliers),\n Detroit Pistons (Detroit, Pistons, Detroit Pistons), \n Milwaukee Bucks (Milwaukee, Bucks, Milwaukee Bucks),\n Chicago Bulls (Chicago, Bulls, Chicago Bulls),\n Indiana Pacers (Indiana Pacers, Indiana, Pacers),\n Atlanta Hawks (Atlanta, Hawks, Atlanta Hawks),\n Charlotte Hornets (Charlotte, Hornets, Charlotte Hornets),\n Washington Wizards (Washington, Wizards, Washington Wizards),\n Denver Nuggets (Denver Nuggets, Denver, Nuggets),\n Portland Trail Blazers (Portland, Trail Blazers, Portland Trailblazers),\n Utah Jazz (Utah Jazz, Utah, Jazz),\n Minnesota Timberwolves (Minnesota, Minnesota TimberWolves, Timberwolves),\n Oklahoma City Thunder (Oklahoma, Oklahoma City, Thunder),\n LA Lakers (LA, Lakers, Los Angelas Lakers),\n Phoenix Suns (Phoenix Suns, Phoenix, Suns),\n Golden State Warriors (Golden State, Warriors, Golden State Warriors),\n LA Clippers (LA, Clippers, LA Clippers),\n Sacramento Kings (Sacramento, Kings, Sacramento Kings),\n San Antonio Spurs (San Antonio, Spurs, San Antonio Spurs),\n Houston Rockets (Houston Rockets, Houston, Rockets),\n New Orleans Hornets (New Orleans, Hornets, New Orleans Hornets),\n Dallas Mavericks (Dallas, Mavericks, Dallas Mavericks),\n Memphis Grizzlies (Memphis, Grizzlies, Memphis Grizzlies),\n", 'timeout'=>10.0, 
-	'onBadChoice' => lambda { say "I didn't get that" },
-	'onChoice'=>lambda { | event |
-   event.onChoice( "Miami Heat", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Orlando Magic", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Boston Celtics", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "New Jersey Nets", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "New York Knicks", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Toronto Raptors", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Cleveland Cavaliers", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Detroit Pistons", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Milwaukee Bucks", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Chicago Bulls", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Indiana Pacers", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Atlanta Hawks", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Charlotte Hornets", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Washington Wizards", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Denver Nuggets", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Portland Trail Blazers", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Utah Jazz", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Minnesota Timberwolves", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Oklahoma City Thunder", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "LA Lakers", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Golden State Warriors", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "LA Clippers", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Sacramento Kings", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "San Antonio Spurs", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Phoenix Suns", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Houston Rockets", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "New Orleans Hornets", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Dallas Mavericks", lambda { say "Your Search Query is [#{event.value}]" } )
-	event.onChoice( "Memphis Grizzlies", lambda { say "Your Search Query is [#{event.value}]" } )
-			}		
-		  }
+    'repeat'      => 2,
+	  'choices'     => "Miami Heat (miami, heat, miami heat, 1), Orlando Magic (Orlando, Magic, Orlando Magic),Boston Celtics (Boston, Boston Celtics, Celtics),Philadelphia Seventy Sixers (Philadelphia, Seventy Sixers, Philadelphia Seventy Sixers),New Jersey Nets (New Jersey, Nets, New Jersey Nets), New York Knicks (New York, New York Knicks, Knicks), Toronto Raptors(Toronto, Raptors, Toronto Raptor), Cleveland Cavaliers (Cleveland, Cavaliers, Cleveland Cavaliers), Detroit Pistons (Detroit, Pistons, Detroit Pistons),  Milwaukee Bucks (Milwaukee, Bucks, Milwaukee Bucks), Chicago Bulls (Chicago, Bulls, Chicago Bulls), Indiana Pacers (Indiana Pacers, Indiana, Pacers), Atlanta Hawks (Atlanta, Hawks, Atlanta Hawks), Charlotte Hornets (Charlotte, Hornets, Charlotte Hornets), Washington Wizards (Washington, Wizards, Washington Wizards), Denver Nuggets (Denver Nuggets, Denver, Nuggets), Portland Trail Blazers (Portland, Trail Blazers, Portland Trailblazers), Utah Jazz (Utah Jazz, Utah, Jazz), Minnesota Timberwolves (Minnesota, Minnesota TimberWolves, Timberwolves), Oklahoma City Thunder (Oklahoma, Oklahoma City, Thunder), LA Lakers (LA, Lakers, Los Angelas Lakers), Phoenix Suns (Phoenix Suns, Phoenix, Suns), Golden State Warriors (Golden State, Warriors, Golden State Warriors), LA Clippers (LA, Clippers, LA Clippers), Sacramento Kings (Sacramento, Kings, Sacramento Kings), San Antonio Spurs (San Antonio, Spurs, San Antonio Spurs), Houston Rockets (Houston Rockets, Houston, Rockets), New Orleans Hornets (New Orleans, Hornets, New Orleans Hornets), Dallas Mavericks (Dallas, Mavericks, Dallas Mavericks), Memphis Grizzlies (Memphis, Grizzlies, Memphis Grizzlies)"
+	  'timeout'     => 10.0, 
+	  'onBadChoice' => lambda { say "I didn't get that" },
+  	'onChoice'    => lambda {| event| say say "Your Search Query is #{event.value}" }
+		}		
+	}
 event= prompt("Welcome to a Tropo Experience, please say your favorite basketball team, and Tropo powered by Ruby will return back the top 2 yahoo search results for you!!!!", options )
 
 if event.name !="hangup"
