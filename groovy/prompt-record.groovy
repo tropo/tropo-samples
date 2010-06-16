c112a101bb09fb64944138203667e6082d718fcd
JustinDupree <jdupree@voxeo.com>
2010-06-16 09:11:16 -0700

    Fixed more typos.

--- groovy/prompt-record.groovy
+++ groovy/prompt-record.groovy
@@ -12,7 +12,7 @@ log("Incoming call info [state:" + currentCall.state() + ", callerID:" + current
 def event=prompt("http://127.0.0.1:8080/beep.wav where are you heading?",
    [repeat:3,record:true, beep:true, silenceTimeout: 3, maxTime:30,timeout:20.03456789,
     onRecord: {event-> say("you said " + event.recordURI );},
-    choices:"1st Floor (first, house wares, 1),\n 2nd Floor (second, bed and bath, 2),\n 3rd Floor (third, sporting goods, 3)", 
+    choices:"1st Floor (first, house wares, 1), 2nd Floor (second, bed and bath, 2), 3rd Floor (third, sporting goods, 3)", 
     onChoice: {event->
       event.onChoice( "1st Floor", { say("Your destination is 1st Floor") } );
       event.onChoice( "2nd Floor", { say("Your destination is 2nd Floor") } );
@@ -20,13 +20,13 @@ def event=prompt("http://127.0.0.1:8080/beep.wav where are you heading?",
     }, 
     onBadChoice: { say("I can not recognize you. Please input again."); }, 
     onTimeout: { say("wait input time out"); }, 
-    onHangup: { print("Disconnected by the peer!<<<<<<<<<<<<<<<<<"); }, 
+    onHangup: { print(">>>>>>>>>>>>>>>>>Disconnected by the peer!<<<<<<<<<<<<<<<<<"); }, 
     onError: { say("You have an error!"); },
     onEvent: {event->
       if(event.name!="hangup"){ say("inner callback got triggered by event " + event.name);}
-      event.onError( { say("000You have an error err! ") } );
-      event.onTimeout( { say("000Wait input time out") } );
-      event.onHangup( { print("000Disconnected by the peer!<<<<<<<<<<<<<<<<<") } );
+      event.onError( { say("You have an error!") } );
+      event.onTimeout( { say("Wait input time out") } );
+      event.onHangup( { print(">>>>>>>>>>>>>>>>Disconnected by the peer!<<<<<<<<<<<<<<<<<") } );
       event.onChoice( "1st Floor", { say("Your destination is 1st Floor") } );
       event.onChoice( "2nd Floor", { say("Your destination is 2nd Floor") } );
       event.onChoice( "3rd Floor", { say("Your destination is 3rd Floor") } ); 
@@ -38,7 +38,7 @@ def event=prompt("http://127.0.0.1:8080/beep.wav where are you heading?",
 
 if(event.name!="hangup"){
   say("run outer call back for event [" + event.name +"," + event.value +"]");
-  event.onError( { say("You have an error err! ") } );
+  event.onError( { say("You have an error!") } );
   event.onTimeout( { say("wait input time out") } );
   event.onChoice( "1st Floor", { say("Your destination is 1st Floor") } );
   event.onChoice( "2nd Floor", { say("Your destination is 2nd Floor") } );
