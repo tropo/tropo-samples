60e9325a81fb2c591ad866a997a79b43fc5db5e2
Justin Dupree <jdupree@voxeo.com>
2010-07-16 16:42:03 -0400

    Edited for content

--- ruby/countdown.rb
+++ ruby/countdown.rb
@@ -7,20 +7,19 @@ options = {:choices => 'yes(yes, 1), no(no,2)',
 		:attempts => 1,
 		:timeout => 15,
 		:voice => 'simon',
-		:onBadChoice =>  lambda { say 'What you say?'},
-		:onTimeout => lambda { say 'Oh god, its too late!'}}
+		:onBadChoice =>  lambda { say 'What?', :voice => 'simon'},
+		:onTimeout => lambda { say 'Oh god, its too late!', :voice => 'simon'}}
 		
-result = ask 'Countdown activated!  Do you want to know how long you have before disaster strikes?', options
+result = ask 'Countdown to 20 12 activated!  Do you want to know how long you have before disaster strikes?', options
 
 if result.name=='choice'
 	case result.value
 	when 'yes'
 		say countdown.to_s + 'days. Prepare yourself!', {:voice => 'simon'}
 	when 'no'
-		say 'I don\'t blame you.', {:voice => 'simon'}
+		say 'I don\'t blame you, ignorance is bliss.', {:voice => 'simon'}
 
 	end
 end
 
 hangup
-	
