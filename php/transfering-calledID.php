132f41f10ac66307300f18e9d77d4357a86388ef
Adam Kalsey <akalsey@cobweb.local>
2010-08-27 09:30:44 -0700

    New sample: Transferring calls based on the number called.

---
+++ php/transfering-calledID.php
@@ -0,0 +1,19 @@
+<?php
+$numbers = array(
+    '9165551212' => '13105551212',
+    '6505551313' => '12065551313',
+  );
+  
+answer();
+if (!array_key_exists($currentCall->calledID, $numbers)) {
+  // This shouldn't be able to happen. But log it and hang up.
+  _log('Error: Could not handle a call to '. $currentCall->calledId);
+} else {
+  $options = array(
+      'playvalue' => 'http://example.com/ring.mp3',
+    );
+  transfer('tel:+' . $numbers[$currentCall->calledID], $options);
+}
+hangup();
+
+?>
