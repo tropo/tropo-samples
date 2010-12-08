b1af5479234676749cd3d6db846affea8cd026d9
JustinDupree <jdupree@voxeo.com>
2010-12-08 13:56:21 -0800

    Removed answer and hangup, fixed transfer to omit choices and adjust syntax.

--- groovy/transfer.groovy
+++ groovy/transfer.groovy
@@ -3,27 +3,22 @@
 // See http://www.tropo.com for more info
 // --------------------------------------------
 
-answer()
 print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + callFactory);
-say("transfering to ");
+say("transferring to ");
 
-def ncall = currentCall.transfer("sip:914074181800@10.6.63.201", [
+def ncall = transfer("sip:914074181800@10.6.63.201", [
         answerOnMedia: false,
-        callerId:      "tel:+14076179024",
+        callerId:      "+14075551212",
         timeout:       60.3456,
         method:        "bridged", // fixed to bridged currently
         playrepeat:    3,
         playvalue:     "Ring... Ring... Ring...",
-        choices:       "1,2,3,4,5,6,7,8,9,0,*,#",
-                onSuccess:     { ncall-> log("*********transfered to $ncall.value.calleeId") },  
-                onError:       { ncall-> log("*********transfer error") },  
-                onTimeout:     { ncall-> log("*********transfer timeout") },  
-                onCallFailure: { ncall-> log("*********transfer failed") },  
-                onChoice:      { ncall-> log("*********transfer canceled") }  
+        onSuccess:     { ncall-> log("*********transfered to $ncall.value.calledID") },  
+        onError:       { ncall-> log("*********transfer error") },  
+        onTimeout:     { ncall-> log("*********transfer timeout") },  
+        onCallFailure: { ncall-> log("*********transfer failed") }  
         ]
 )
 
 print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + ncall);
-
-hangup()
 
