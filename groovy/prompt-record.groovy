c5a3d3551b3a80bb9b41cd51518a6cec4f1da951
JustinDupree <jdupree@voxeo.com>
2010-12-08 13:13:40 -0800

    Removed answer and hangup, changed repeat to attempts.

--- groovy/prompt-record.groovy
+++ groovy/prompt-record.groovy
@@ -3,14 +3,13 @@
 // See http://www.tropo.com for more info
 // --------------------------------------------
 
-answer()
 
 await(1000) // wait for 1 second. PLEASE USE await in Groovy
 log("This is a log demo")
 log("Incoming call info [state:" + currentCall.state() + ", callerID:" + currentCall.callerID + ", callerName:" + currentCall.callerName + "]")
 
 def event=ask("http://example.com/beep.wav where are you heading?",
-   [repeat:3,record:true, beep:true, silenceTimeout: 3, maxTime:30, timeout:20.03456789,
+   [attempts:3, record:true, beep:true, silenceTimeout: 3, maxTime:30, timeout:20.03456789,
     onRecord: {event-> say("you said " + event.recordURI );},
     choices:"1st Floor (first, house wares, 1), 2nd Floor (second, bed and bath, 2), 3rd Floor (third, sporting goods, 3)", 
     onChoice: {event->
@@ -38,7 +37,7 @@ def event=ask("http://example.com/beep.wav where are you heading?",
 
 if(event.name!="hangup"){
   say("run outer call back for event [" + event.name +"," + event.value +"]");
-  event.onError( { say("You've have an error!") } );
+  event.onError( { say("You have an error!") } );
   event.onTimeout( { say("wait input time out") } );
   event.onChoice( "1st Floor", { say("Your destination is 1st Floor") } );
   event.onChoice( "2nd Floor", { say("Your destination is 2nd Floor") } );
@@ -46,7 +45,6 @@ if(event.name!="hangup"){
   event.onBadChoice( { say("I can not recognize you") } ); 
   event.onRecord( {recordEvent-> say("you said " + recordEvent.recordURI );});
   say("Thanks for testing Groovy on the Tropo platform");
-  hangup()
 }
 else{
   log(">>>>>>>>>>>>>>>Disconnected by the peer!<<<<<<<<<<<<<<<<<");
