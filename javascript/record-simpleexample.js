1a93575e2e0299eebdd194fa5ced03a9e91e7d56
Stève Sfartz <ObjectIsAdvantag@users.noreply.github.com>
2016-09-25 01:04:04 +0200

    Update record-simpleexample.js

--- javascript/record-simpleexample.js
+++ javascript/record-simpleexample.js
@@ -8,11 +8,19 @@
 // Simple example of recording
 // --------------------------------------------
 
-event=record("Leave your message at the beep.  Thanks!",
+record("Leave your message after the beep. Thanks!",
   {
     beep:true, silenceTimeout: 5, maxTime:60, timeout:10,
-    onRecord:function(event ) 
-        { say("you said " + event.recordURI )  }
+    onRecord:function(event) 
+        { 
+          // Note that by default, Tropo stores the recorded audio file on its runtime server. 
+          // This temporary file cannot be accessed externall: only your script will be able to read the audio file as it gets executed inside tropo hosting environment. 
+          //
+          // If you want to keep the audio file in a safe place, use the recordURI parameter.
+          //     - to specify your Tropo files directory, specify recordURI:"ftp://ftp.tropo.com" and your user/password,
+          //     - or simply push the file to any external service: recordURI:"https;//..." or "ftp://..."
+          //
+          // for more info, check: https://www.tropo.com/docs/hosting-debugging-logs/audio
+          say("you said " + event.value );  
+        }
   } );
-
-log( "event.recordURI = " + event.recordURI );
